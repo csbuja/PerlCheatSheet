a98cd50d828bc091df5a4cf2055af99862d19735
csbuja <csbuja@umich.edu>
2021-02-11 14:51:15 -0800

    more perl

--- helloworld.pl
+++ helloworld.pl
@@ -62,3 +62,33 @@ my $s;
 print chomp($s = <STDIN>), "\n";
 print $s,"\n";
 print $s, "\n";
+
+
+#lists
+print(("eh",2,"oh")[2],"\n");
+print((0..2)[0,1],"\n"); # list slicing with a range
+print(a..z,"\n");
+
+#Arrays
+#source: https://www.perltutorial.org/perl-array/
+my @weekdays = qw(Monday Tuesday Wednesday Thursday Friday);
+print("@weekdays","\n");
+
+print($weekdays[0],"\n");
+print(@weekdays[0,1],"\n");
+
+#array length example
+my $count = scalar @weekdays;
+print($count, "\n"); #5 weekdays in the week
+
+
+#modifying array
+$weekdays[0] = "M";
+@weekdays[1..4] = qw(T W T F); # note the @ sign instead of the $ sign
+print("@weekdays","\n");
+
+#just like python, you can leverage an array as a stack
+my @stack = (1,2,3);
+push(@stack, "4h");
+
+print(@stack,"\n")
